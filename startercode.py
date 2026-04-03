2896df7bd40031f8aa97a5a1cdc4c4e20fe1a80d
Gabby <gabby@GabbydeMacBook-Air.local>
2026-04-03 13:49:06 -0400

    update_cache

--- startercode.py
+++ startercode.py
@@ -73,7 +73,14 @@ def search_breed(breed_id):
         JSON body as a dict (with a top-level 'data' key on success), OR None if the
         request failed or the response does not represent a successful breed lookup.
     """
-    pass
+    url = f"https://dogapi.dog/api/v2/breeds/{breed_id}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json(), url
+        return None
+    except:
+        return None
 
 
 def update_cache(breed_ids, cache_file):
@@ -90,7 +97,27 @@ def update_cache(breed_ids, cache_file):
         A string: "Cached data for {percentage}% of breeds",
         where percentage = (successful_new_adds / len(breed_ids)) * 100.
     """
-    pass
+    cache = load_json(cache_file)
+
+    successful_new_adds = 0
+
+    for b in breed_ids:
+        url = f"https://dogapi.dog/api/v2/breeds/{b}"
+
+        if url in cache: #Skip requests for breeds whose URL is already in the cache
+            continue
+
+        result = search_breed(b)
+
+        if result is not None:
+            data, url = result
+            cache[url] = data
+            successful_new_adds += 1
+
+    create_cache(cache, cache_file)
+
+    percentage = (successful_new_adds / len(breed_ids)) * 100
+    return f"Cached data for {percentage}% of breeds"
 
 
 def get_longest_lifespan_breed(cache_file):
